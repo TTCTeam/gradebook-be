284b701cbe0d42f1a52f99041f9c99f676c2f1ca
Hà Minh Cường <55825733+haminhcuong@users.noreply.github.com>
2021-12-22 17:54:45 +0700

    Update router.js

--- components/assigments/router.js
+++ components/assigments/router.js
@@ -1,13 +1,28 @@
-import express from "express"
-import { getAssignmentBoard, getStudentList, getUserAssignmentListInAssignmentId, uploadAssignmentsListByAssignmentId, uploadStudentList } from "./assignmentController.js";
+import express from 'express';
+import {
+  publicAssignment,
+  getAssignmentBoard,
+  getStudentList,
+  getUserAssignmentListInAssignmentId,
+  uploadAssignmentsListByAssignmentId,
+  uploadStudentList,
+} from './assignmentController.js';
 const assigmentsRouter = express.Router();
 
-assigmentsRouter.get('/',(req,res)=>{res.send({message:"lalala"})});
-assigmentsRouter.get('/public/:assignmentId',publicAssignment);
-assigmentsRouter.get('/:courseId',getAssignmentBoard);
-assigmentsRouter.put('/:courseId/studentlist',uploadStudentList);
-assigmentsRouter.get('/:courseId/studentlist',getStudentList);
-assigmentsRouter.put('/:assignmentId/:courseId',uploadAssignmentsListByAssignmentId);
-assigmentsRouter.get('/:assignmentId/:courseId',getUserAssignmentListInAssignmentId);
+assigmentsRouter.get('/', (req, res) => {
+  res.send({ message: 'lalala' });
+});
+assigmentsRouter.get('/public/:assignmentId', publicAssignment);
+assigmentsRouter.get('/:courseId', getAssignmentBoard);
+assigmentsRouter.put('/:courseId/studentlist', uploadStudentList);
+assigmentsRouter.get('/:courseId/studentlist', getStudentList);
+assigmentsRouter.put(
+  '/:assignmentId/:courseId',
+  uploadAssignmentsListByAssignmentId
+);
+assigmentsRouter.get(
+  '/:assignmentId/:courseId',
+  getUserAssignmentListInAssignmentId
+);
 
 export default assigmentsRouter;
