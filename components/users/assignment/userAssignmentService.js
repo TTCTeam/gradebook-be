d9cd483eca4973adfcdc79e3d97c6cb3c458a20b
HUYNH HO THANH TRA <hothanhtrahuynh@github.com>
2021-12-22 20:27:43 +0700

    Fix get user assignments

--- components/users/assignment/userAssignmentService.js
+++ components/users/assignment/userAssignmentService.js
@@ -7,22 +7,33 @@ import UserAssignment from "./userAssignmentModel.js";
 async function getAllAssignmentsOfUser(userId, courseId) {
   const user = await User.findOne({ where: { id: userId } });
   const studentId = user.username;
+  console.log(user.toJSON());
+  console.log(studentId, courseId);
 
   const courseMember = await CourseMember.findOne({
     where:{
       studentId, courseId
     }
   })
-  console.log(courseMember);
+  let assignmentsResult=[];
   if (courseMember) {
     const assignments = await courseMember.getAssignments({
       where:{
         status: AssignmentStatus.PUBLIC
       }
     });
+    for( const assignment of assignments){
+     const userAssignment = await assignment.getSubmissions({
+        where:{
+          courseMemberId: courseMember.id
+        },
+        attributes:['id','point','assignmentId']
+      });
+      assignmentsResult.push(userAssignment);
+    }
     console.log(assignments,'user assignment');
-    if (assignments) {
-      return assignments;
+    if (assignmentsResult) {
+      return assignmentsResult;
     }
   }
   return [];
